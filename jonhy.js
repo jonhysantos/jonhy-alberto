b9730dc810f7a13d2e66fbb1e4597b69636e6ab4
Jonhy Santos <Jonhy1905@hotmail.com>
2024-04-05 18:00:08 -0300

    Apliquei um efeito de transition.

--- jonhy.js
+++ jonhy.js
@@ -1,3 +1,18 @@
+const myObserver = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach((element) => myObserver.observe(element))
+
+
 function clickMenu(){
     if (itens.style.left == '-500px'){
         itens.style.left = '-10px'
